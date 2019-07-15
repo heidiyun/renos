2b28d8ab6e7a65267f28cec675f8b2dec7b4da5b
hyunwoo <h.hyunwoo@gmail.com>
2019-07-16 02:07:48 +0900

    file layout 크기 조절

--- src/views/drive/drive.vue
+++ src/views/drive/drive.vue
@@ -1,50 +1,35 @@
 <template>
-  <v-container fluid ma-0 pa-0 style="overflow-y:auto; height:100vh;">
+  <v-container fluid
+               ma-0
+               pa-0
+               style="overflow-y:auto; height:100vh;">
     <v-layout>
       <v-flex>
         <div class="content-container">
-          <div class="category" style="padding: 0px 36px;">
+          <div class="category"
+               style="padding: 0px 36px;">
             <div class="content-title">빠른 액세스</div>
           </div>
           <div class="content">
-            <v-layout class="file-card-layout" wrap style="width:100%" pa-2>
-              <v-flex
-                style="width=325px; height : 250px;"
-                xl4
-                lg3
-                md4
-                sm6
-                xs12
-                v-for=" (file,i) in fileList"
-                :key="i"
-                px-2
-                py-3
-              >
+            <v-layout class="file-card-layout"
+                      wrap
+                      style="width:100%"
+                      pa-2>
+              <v-flex style="width=325px; height : 250px;"
+                      xl2
+                      lg3
+                      md4
+                      sm6
+                      xs12
+                      v-for=" (file,i) in fileList"
+                      :key="i"
+                      px-2
+                      py-3>
                 <file-card :file="file"></file-card>
               </v-flex>
             </v-layout>
           </div>
-          <div class="category" style="padding: 0px 36px;">
-            <div class="content-title">파일</div>
-          </div>
-          <div class="content">
-            <v-layout class="file-card-layout" wrap style="width:100%" pa-2>
-              <v-flex
-                style="width=325px; height : 250px;"
-                xl4
-                lg3
-                md4
-                sm6
-                xs12
-                v-for=" (file,i) in fileList"
-                :key="i"
-                px-2
-                py-3
-              >
-                <file-card :file="file"></file-card>
-              </v-flex>
-            </v-layout>
-          </div>
+
         </div>
       </v-flex>
     </v-layout>
